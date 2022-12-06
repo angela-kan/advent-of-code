8437d3da3d7d4308e940ac843f9443f970c17033
ak561 <ak561@duke.edu>
2022-12-05 22:27:48 -0500

    Day 2 part 2

--- score.py
+++ score.py
@@ -2,7 +2,7 @@ import sys
 import re
 
 def main():
-    get_score()
+    get_score_correct()
 
 def get_score():
     combos = {'AX': 4,
@@ -22,5 +22,24 @@ def get_score():
         pattern = re.compile(r'\s+')
         score += combos[re.sub(pattern, '', line)]
     print(f'The score given by the guide is {score}')
+
+def get_score_correct():
+    combos = {'AX': 3,
+              'AY': 4,
+              'AZ': 8,
+              'BX': 1,
+              'BY': 5,
+              'BZ': 9,
+              'CX': 2,
+              'CY': 6,
+              'CZ': 7}
+    score = 0
+
+    filename = sys.argv[1]
+    f = open(filename, 'r')
+    for line in f.readlines():
+        pattern = re.compile(r'\s+')
+        score += combos[re.sub(pattern, '', line)]
+    print(f'The correct score given by the guide is {score}')
 
 main()
