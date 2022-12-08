281b2edb6c4d9e91d75fefd6130ecddda7f9192e
ak561 <ak561@duke.edu>
2022-12-08 12:53:13 -0500

    day 3 pt 2

--- priorities.py
+++ priorities.py
@@ -1,7 +1,7 @@
 import sys
 
 def main():
-    get_priorities()
+    get_badges()
 
 def get_priorities():
     filename = sys.argv[1]
@@ -21,5 +21,28 @@ def get_priorities():
                 break
             i+=1
     print(f'The total sum of priorities is {total}')
+
+def get_badges():
+    filename = sys.argv[1]
+    f = open(filename, "r")
+    total = 0
+
+    lines = f.readlines()
+    for i in range(0,len(lines), 3):
+        elf1 = set(lines[i].strip())
+        elf2 = set(lines[i+1].strip())
+        elf3 = set(lines[i+2].strip())
+
+        badge = elf1.intersection(elf2).intersection(elf3).pop()
+        total += get_priority(badge)
+
+    print(f'The total priorities of the badges is {total}')
+
+def get_priority(item):
+    if ord(item) > 90:
+        return ord(item)-96
+    else:
+        return ord(item)-64+26
+        
 
 main()
