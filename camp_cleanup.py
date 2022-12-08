a888784efde2ef863e19df76f38ec0feb3c61865
ak561 <ak561@duke.edu>
2022-12-08 14:14:38 -0500

    day 4 pt 2

--- camp_cleanup.py
+++ camp_cleanup.py
@@ -2,6 +2,7 @@ import sys
 
 def main():
     get_contained_pairs()
+    get_overlaps()
 
 def get_contained_pairs():
     filename = sys.argv[1]
@@ -17,4 +18,21 @@ def get_contained_pairs():
         else:
             overlaps += 1
     print(f'The number of contained pairs is {overlaps}.')
+
+def get_overlaps():
+    filename = sys.argv[1]
+    f = open(filename, 'r')
+    overlaps = 0
+    for line in f.readlines():
+        pair = [slot.split('-') for slot in line.strip().split(',')]
+        pair = [[int(x) for x in i] for i in pair]
+        pair.sort()
+    
+        if pair[1][0] > pair[0][1]:
+            continue
+        else:
+            overlaps += 1
+
+    print(f'The number of overlapping pairs is {overlaps}.')
+
 main()
