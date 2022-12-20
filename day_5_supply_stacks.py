9455bd9b41838e7aee962cb1c1b23804308390ba
ak561 <ak561@duke.edu>
2022-12-19 22:55:45 -0700

    day 5 pt 2

--- day_5_supply_stacks.py
+++ day_5_supply_stacks.py
@@ -1,10 +1,12 @@
+#TRY WITH QUEUE?
+
 import sys
 # import queue
 
 def main():
-    simulation()
+    simulation2()
 
-def simulation():
+def simulation1():
     filename = sys.argv[1]
     f = open(filename, 'r')
 
@@ -26,6 +28,34 @@ def simulation():
         line = line.split()
         for i in range(int(line[1])):
             stacks[int(line[5])].insert(0,stacks[int(line[3])].pop(0))
+
+    top_blocks = ''
+    for i in range(1,len(stacks)+1):
+        top_blocks += stacks[i][0]
+    print(f'The top layer of blocks is {top_blocks}')
+
+def simulation2():
+    filename = sys.argv[1]
+    f = open(filename, 'r')
+
+    stacks = {}
+    line = f.readline()
+    while line != '\n':
+        for i in range(0, len(line), 4):
+            if line[i:i+3] != '   ':
+                val = (i//4) + 1
+                if val in stacks:
+                    stacks[val].append(line[i+1])
+                else:
+                    stacks[val] = [line[i+1]]        
+        line = f.readline()
+
+    instructions = f.readlines()
+
+    for line in instructions:
+        line = line.split()
+        stacks[int(line[5])] = stacks[int(line[3])][:int(line[1])] + stacks[int(line[5])]
+        stacks[int(line[3])] = stacks[int(line[3])][int(line[1]):]
 
     top_blocks = ''
     for i in range(1,len(stacks)+1):
