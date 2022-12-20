8f0a3681920f2098537aaf3153ce30ed9017bd7b
ak561 <ak561@duke.edu>
2022-12-20 13:46:33 -0700

    day 6 pt 2

--- day_6_tuning_trouble.py
+++ day_6_tuning_trouble.py
@@ -1,9 +1,11 @@
 import sys
 
 def main():
-    print(f'The first start of packet market is at {detect_marker()}.')
+    print(f'The first start of packet market is at {detect_packet_marker()}.')
+    print(f'The first start of message market is at {detect_message_marker()}.')
 
-def detect_marker():
+
+def detect_packet_marker():
     filename = sys.argv[1]
     f = open(filename, 'r')
     signal = f.readline()
@@ -13,6 +15,17 @@ def detect_marker():
     for i in range(len(signal)):
         if len(signal[i:i+4]) == len(set(signal[i:i+4])):
             return i+4
+
+def detect_message_marker():
+    filename = sys.argv[1]
+    f = open(filename, 'r')
+    signal = f.readline()
+    
+    nums = {}
+
+    for i in range(len(signal)):
+        if len(signal[i:i+14]) == len(set(signal[i:i+14])):
+            return i+14
 
 
 main()
